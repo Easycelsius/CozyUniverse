0b922a31037726d498148e7c0cef46842fd60160
Easycelsius <77523551+Easycelsius@users.noreply.github.com>
2022-01-07 14:48:35 +0900

    [add] Android shopping

---
+++ Android/shopping/app/src/main/java/cozyuniverse/android/shopping/MainActivity.java
@@ -0,0 +1,14 @@
+package cozyuniverse.android.shopping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
